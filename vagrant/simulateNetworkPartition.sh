270d0b91635d2ab6c9c04ed3f06cc446a7d7d9bb
wadewaldron <wade.waldron@boldradius.com>
2018-09-24 13:21:46 -0600

    Update the "simulateNetworkPartition.sh" to use iptables firewall rules.

--- vagrant/simulateNetworkPartition.sh
+++ vagrant/simulateNetworkPartition.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-sudo ip link set enp0s8 down
+# Create a Firewall rule to drop all requests from all interfaces.
+sudo iptables -A INPUT -j DROP
 
 sleep 30
 
-sudo ip link set enp0s8 up
+# Remove all Firewall rules to restore communication.
+sudo iptables -F
